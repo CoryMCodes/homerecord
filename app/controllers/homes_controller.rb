10c515dd193d3a31d4bb636ee5145f9d6f103b76
Cory Musick <cory.musick@alphaomega.com>
2026-07-17 16:45:05 -0400

    Add timeline entry type filters

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -19,10 +19,31 @@ class HomesController < ApplicationController
 
   def show
     @home = current_account.homes.find(params[:id])
-    @timeline_entries = @home.entries.order(occurred_on: :desc, id: :desc)
+    @entry_type_filter = filtered_entry_type
+    @timeline_filter_items = timeline_filter_items
+    @timeline_entries = @home.entries
+    @timeline_entries = @timeline_entries.where(entry_type: @entry_type_filter) if @entry_type_filter.present?
+    @timeline_entries = @timeline_entries.order(occurred_on: :desc, id: :desc)
   end
 
   private
+
+  def filtered_entry_type
+    params[:entry_type].presence_in(Entry::ENTRY_TYPES)
+  end
+
+  def timeline_filter_items
+    [
+      { label: "All", href: home_path(@home), active: @entry_type_filter.blank? },
+      *Entry::ENTRY_TYPES.map do |entry_type|
+        {
+          label: entry_type.humanize,
+          href: home_path(@home, entry_type: entry_type),
+          active: @entry_type_filter == entry_type
+        }
+      end
+    ]
+  end
 
   def home_params
     params.require(:home).permit(:name, :address, :home_type)
